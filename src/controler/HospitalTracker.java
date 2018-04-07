f62a37ae2b1f884c39aca3bfc3ec1de8601ca1a6
jaguyard <jean-adrien.guyard@edu.ece.fr>
2018-04-07 20:00:33 +0200

    Fenetre login et choix + ActionListener
    Premières fonctionnalitées pour passer d'un JPanel à un autre en cliquant sur un bouton (à l'aide d'ActionListener)

--- src/controler/HospitalTracker.java
+++ src/controler/HospitalTracker.java
@@ -5,19 +5,51 @@
  */
 package controler;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import vue.Fenetre;
+
 /**
  *
  * @author Antoine
  */
-public class HospitalTracker {
-
+public class HospitalTracker implements ActionListener{
+    private Fenetre fen = new Fenetre();
+    
+    public HospitalTracker(Fenetre fen){
+        this.fen = fen;
+        this.fen.getLog().addActionListener(this);
+        this.fen.getModif().addActionListener(this);
+        this.fen.getRecherche().addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource().equals(fen.getLog())){
+            fen.nextContenu(1);
+            //System.out.println("coucou");
+        }
+        if(ae.getSource().equals(fen.getModif())){
+            fen.nextContenu(3);
+        }
+        if(ae.getSource().equals(fen.getRecherche())){
+            fen.nextContenu(2);
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Fenetre fen = new Fenetre();
+        HospitalTracker a = new HospitalTracker(fen);
+        fen.affiche();
+        
     }
     
     // Methodes 
+
+    
     
 }
--- src/vue/Fenetre.java
+++ src/vue/Fenetre.java
@@ -1,22 +1,100 @@
 package vue;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * Contient la classe Fenetre qui sert de base à l'interface graphique.
  */
 public class Fenetre extends JFrame{
     // Variables
+    private CardLayout c;
+    private JPanel contenu;
+    private String[] listContenu = {"login","choix","recherche","modification"};
+    
+    // on crée les panneaux
+    private JPanel contenant;
+    private JPanel login;
+    private JPanel choix;
+    private JPanel recherche;
+    private JPanel modification;
+        
+    //On crée les boutons
+    private JButton log;
+    private JButton modif;
+    private JButton requete;
+    private JButton graphe;
     
     // Constructeurs
     public Fenetre()
     {
         super();
-        this.setTitle("HospitalTracker");
-        this.setSize(400, 400);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        setTitle("HospitalTracker");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    
+    // on crée les panneaux
+        login = new JPanel();
+        choix = new JPanel();
+        recherche = new JPanel();
+        modification = new JPanel();
+        
+    //On crée les boutons
+        log = new JButton("LOGIN");
+        modif = new JButton("MODIFIER");
+        requete = new JButton("REQUETE");
+        graphe = new JButton("GRAPHE");
+    
     }
     
     // Methodes
+    public void affiche(){
+        //on remplit les panneaux
+        login.add(log);
+        choix.add(modif);
+        choix.add(requete);
+        choix.add(graphe);
+        setContentPane(login);
+        setVisible(true);
+      
+        
+    }
+
+
+public JButton getLog(){
+    return log;
+}
+
+public JButton getModif(){
+    return modif;
+}
+
+public JButton getRecherche(){
+    return requete;
+}
+
+public JButton getGraphe(){
+    return graphe;
+}
+
+public void nextContenu(int c){
+    switch(c)
+    {
+        case 1:
+            setContentPane(choix);
+            break;
+        case 2:
+            setContentPane(recherche);
+            break;
+        case 3:
+            setContentPane(modification);
+            break;
+        default:
+            setContentPane(login);
+    }
+    revalidate();
+}
+
 }
