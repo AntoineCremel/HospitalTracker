53f87b98cd212d4f4c58f7f1d02361c4cccf0fdb
Antoine <antoine.cremel@gmail.com>
2018-04-21 18:19:09 +0200

    Added function to get statistics about doctor's specialties

--- src/controler/LectureBDD.java
+++ src/controler/LectureBDD.java
@@ -287,6 +287,24 @@ public abstract class LectureBDD {
         Fonction permettant de récupérer le nombre de médecin pour chaque
         spécialité.
         */
+        // 0 Variables
+        String requete;
+        ArrayList<ArrayList<String>> lecture;
+        Statistiques retour = new Statistiques();
+        
+        // 1 Création de la requete
+        requete = "SELECT COUNT(numero),specialite FROM docteur GROUP BY"
+                + "specialite";
+        // 2 Récupération du résultat
+        lecture = connex.remplirChampsRequete(requete);
+        // 3 Remplissage du resultat dans une variable de type statistiques
+        for(int i = 0; i<lecture.size(); i++)
+        {
+            retour.addElement(lecture.get(i).get(1), 
+                    Integer.parseInt(lecture.get(i).get(0)));
+        }
+        
+        return retour;
     }
     /// Support de l'écriture
     /*
