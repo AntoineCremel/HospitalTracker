c89ef14f205c6438ee8bd69b3f337120ee08fa9e
Antoine <antoine.cremel@gmail.com>
2018-04-21 12:47:11 +0200

    Defined new class container to hold values for a camemebert graph

---
+++ src/controler/container/Statistiques.java
@@ -0,0 +1,15 @@
+package controler.container;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *  Classe qui sera utilisée comme variable de retour pour la construction de
+ * camemberts.
+ * 
+ * @author Antoine
+ */
+public class Statistiques {
+    private List categories;
+    private List numbers;
+}
